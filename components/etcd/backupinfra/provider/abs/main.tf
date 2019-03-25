0f41898104f00f20edbbccdb010a581827986300
Johannes Aubart <johannes.aubart@sap.com>
2019-03-25 16:37:31 +0100

    add Azure provider

---
+++ components/etcd/backupinfra/provider/abs/main.tf
@@ -0,0 +1,51 @@
+provider "azurerm" {
+  client_id       = "${var.CLIENT_ID}"
+  client_secret   = "${var.CLIENT_SECRET}"
+  tenant_id       = "${var.TENANT_ID}"
+  subscription_id = "${var.SUBSCRIPTION_ID}"
+  version         = "=1.22.0"
+}
+
+//=====================================================================
+//= ABS bucket
+//=====================================================================
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.RESOURCE_GROUP}"
+  location = "${var.REGION}"
+}
+
+resource "azurerm_storage_account" "storageAccount" {
+  name                     = "${var.STORAGE_ACCOUNT_NAME}"
+  location                 = "${var.REGION}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_kind             = "BlobStorage"
+  access_tier              = "Hot"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  enable_blob_encryption   = true
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "${var.BUCKETNAME}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  storage_account_name  = "${azurerm_storage_account.storageAccount.name}"
+  container_access_type = "private"
+}
+
+//=====================================================================
+//= Output variables
+//=====================================================================
+
+output "bucketName" {
+  value = "${azurerm_storage_container.container.name}"
+}
+
+output "storageAccountName" {
+  value = "${azurerm_storage_account.storageAccount.name}"
+}
+
+output "storageAccessKey" {
+  sensitive = true
+  value     = "${azurerm_storage_account.storageAccount.primary_access_key}"
+}
