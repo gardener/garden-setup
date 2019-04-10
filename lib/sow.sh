4145a0c68d0f1167ca512cac4bdb23ffa0e03aa5
Uwe Krueger <uwe.krueger@sap.com>
2019-04-10 11:52:11 +0200

    support new command help feature of sow

--- lib/sow.sh
+++ lib/sow.sh
@@ -35,6 +35,14 @@ CMD_k() {
   kubectl --kubeconfig "$kubeconfig" "$@"
 }
 
+HELP_k()
+{
+  echo "k [#] <args>:kubectl for base cluster"
+  echo " [#]:first arument might be the number of the base cluster"
+  echo " :if multiple base clusters are used"
+  echo " <args>:regular kubectl arguments and options"
+}
+
 CMD_kv() {
   local kubeconfig="$EXPORT/kube-apiserver/kubeconfig"
   if [ ! -f "$kubeconfig" ]; then
@@ -43,21 +51,49 @@ CMD_kv() {
   kubectl --kubeconfig "$kubeconfig" "$@"
 }
 
+HELP_kv()
+{
+  echo "kv <args>:kubectl for nodeless garden cluster"
+  echo " <args>:regular kubectl arguments and options"
+}
+
 CMD_kvg() {
   CMD_kv -n garden "$@"
+}
+
+HELP_kvg()
+{
+  echo "kvg <args>:kubectl for garden namespace of nodeless garden cluster"
 }
 
 CMD_kg() {
   CMD_k -n garden "$@"
 }
 
+HELP_kg()
+{
+  echo "kg:kubectl for garden namespace of base cluster"
+}
+
 CMD_url() {
   cat "$EXPORT/dashboard/dashboard_url"
+}
+
+HELP_url()
+{
+  echo "url:show the dashbord url for installed garden"
 }
 
 # dummy method to easily execute a command within the sow image
 CMD_exec() {
   bash -c "$@"
+}
+
+HELP_exec()
+{
+  echo "exec <command>:execute shell command"
+  echo ":useful fow executing commands in sow docker container"
+  echo ":when using the sow docker wrapper"
 }
 
 # convert GCP kubeconfig to basic auth format
@@ -88,3 +124,9 @@ CMD_convertkubeconfig() {
   local tmp=$(spiff merge --json "$kubeconfig" | jq -r '.users[0].user={username:"'"$username"'",password:"'"$password"'"}')
   echo -n "$tmp" > "$kubeconfig"
 }
+
+HELP_convertkubeconfig()
+{
+  echo "convertkubeconfig:convert kubeconfig of GKE clusters"
+}
+
