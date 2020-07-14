e5c91497c53b25efc294f29aa5dc8ae997281ad8
Maximilian Geberl <48486938+dergeberl@users.noreply.github.com>
2020-07-14 16:21:26 +0200

    remove unsupported argument Version (#261)

--- components/etcd/backupinfra/provider/swift/main.tf
+++ components/etcd/backupinfra/provider/swift/main.tf
@@ -20,7 +20,6 @@ provider "openstack" {
   auth_url         = var.AUTH_URL
   domain_name      = var.DOMAIN_NAME
   user_domain_name = var.USER_DOMAIN_NAME
-  Version          = "=1.28"
 }
 
 //=====================================================================
