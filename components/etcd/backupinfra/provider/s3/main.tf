d132a38ea1d1d8cde20c832d2adcecce6a86bc6b
Johannes Aubart <johannes.aubart@sap.com>
2023-02-27 10:46:21 +0100

    Upgrade Gardener and dashboard (#1017)
    * [ci:component:github.com/gardener/gardener:v1.62.3->v1.64.1] (#1012)
    
    * Upgrade github_com_gardener_gardener
    
    from v1.62.3 to v1.64.1
    
    ---------
    
    Co-authored-by: Johannes Aubart <johannes.aubart@sap.com>
    
    * adapt setup to Gardener v1.64
    
    * upgrade default k8s versions and machine images
    
    * adapt shoot spec for v1.25 clusters
    
    * upgrade Gardener Dashboard to 1.67.0
    
    * Upgrade github_com_gardener_sow (#1015)
    
    from 3.6.0 to 3.7.0
    
    Co-authored-by: gardener-robot-ci-1 <gardener.ci.user@gmail.com>
    
    * upgrade AWS terraform provider
    
    ---------
    
    Co-authored-by: Gardener CI Robot 3 <55584046+gardener-robot-ci-3@users.noreply.github.com>
    Co-authored-by: gardener-robot-ci-1 <gardener.ci.user@gmail.com>

--- components/etcd/backupinfra/provider/s3/main.tf
+++ components/etcd/backupinfra/provider/s3/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
   access_key = var.ACCESS_KEY
   secret_key = var.SECRET_KEY
   region     = var.REGION
-  version    = "=3.30"
+  version    = "=3.76.1"
 }
 
 //=====================================================================
